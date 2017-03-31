32bf76cefd71df9c95d2c58879392bddb566cee8
mitakeck <mitake.ck@gmail.com>
2017-04-01 01:06:42 +0900

    update Formula

--- oreillyfreebook.rb
+++ oreillyfreebook.rb
@@ -1,11 +1,11 @@
 class Oreillyfreebook < Formula
   desc "O'Reilly free ebook downloder"
-  homepage ""
   url "https://github.com/mitakeck/oreillyfreebook/releases/download/v0.0.4/oreillyfreebook_darwin_amd64"
   sha256 "01c491bf70b15a3e26443635fe411a3556190a7ac4034fb9569a07849cc443d3"
 
   def install
-  	bin.install 'oreillyfreebook-64'
+    system "mv", "oreillyfreebook-64 oreillyfreebook"
+  	bin.install "oreillyfreebook"
   end
 
   test do
